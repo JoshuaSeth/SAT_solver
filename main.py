4c70a61e1a9c6f8afc41ce463f3ff5f3f4cbb271
JoshuaSeth <SB@accette.eu>
2021-11-10 10:32:09 +0100

    more comments and clarity

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 from SAT_solver import SAT_solve
 from SAT_helper_functions import *
 
-# opening files
+# opening files any of these objects can be chosen to run the sat solver on
 # Satisfiable assignment of variables satifies all these rules
 sudoku_rules_path = "dimac_files/sudoku-rules.txt"
 # Example sudoku setup to test with
@@ -21,16 +21,6 @@ sudoku_rules_dimac = open(sudoku_rules_path, "r")
 sudoku_example = open(sudoku_example_path, "r")
 
 
-"""This cell contains some testing with all the foredefined formulae. """
-# Test clauses for debugging
-test_clauses = read_cnf_from_dimac(test_problem_3)
-if has_empty_clause(test_clauses, log_level=3):
-    print(
-        "File already has empty clauses. Will never terminate. Something wrong with file loading"
-    )
-    sys.exit()
-
-
 # Load sudoku rules and a sudoku
 cnf_formula = read_cnf_from_dimac(sudoku_rules_path)
 example_sudoku_clauses = read_cnf_from_dimac(sudoku_example_path)
@@ -42,5 +32,3 @@ cnf_formula.extend(example_sudoku_clauses)
 # Try some sat solving
 sat = SAT_solve(cnf_formula, log_level=1)
 print(sat)
-
-# When implementing DPLL let's see if this is a smart representation (var lookup-wise) otherwise will modify
