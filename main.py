084f69007a2be56e3e595424009aa04ade12ca62
JoshuaSeth <SB@accette.eu>
2021-11-17 08:22:58 +0100

    added rick's main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 """main.py: The main file from which we run our experiments and tests. For example here we import a dimacs and run it through the SAT solver we con import or apply some helper functions on it."""
-
 import sys
 from SAT_solver import SAT_solve
 from SAT_helper_functions import *
+import glob
+import datetime
 
 # opening files any of these objects can be chosen to run the sat solver on
 # Satisfiable assignment of variables satifies all these rules
@@ -28,8 +29,22 @@ example_sudoku_clauses = read_cnf_from_dimac(sudoku_example_path)
 # Merge the sudoku clauses with the game rules
 cnf_formula.extend(example_sudoku_clauses)
 
-voorbeeld = read_cnf_from_dimac(test_problem_3)
+voorbeeld = read_cnf_from_dimac(test_problem_4)
 
+start_time = datetime.datetime.now()  # record time before program is executed
 # Try some sat solving
-sat = SAT_solve(voorbeeld, log_level=0)
+sat = SAT_solve(cnf_formula, log_level=1)
 print(sat)
+
+# Reading multiple sudoku.txt files. Assumes we name our files 'sudoku_*.txt' where * = a number for each individual sudoku.
+# Also assumes 'sudoku_*.txt' are located in a folder named 'cnf_sudokus'
+"""
+sudoku_path = 'cnf_sudokus'
+for path in glob.iglob(sudoku_path + 'sudoku_*.txt'):
+    SAT_solve(cnf_formula, log_level = 0)
+"""
+delta_time = (
+    datetime.datetime.now() - start_time
+)  # check and save time after the code is ran minus time before the code is ran.
+# delta_time is the time it took to complete
+print(delta_time)
